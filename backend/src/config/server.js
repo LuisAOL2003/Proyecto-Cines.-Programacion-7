27d7daa87c4a1ff224ad477badbdf83e915b9bfa
Luis Ojeda <luisojelopez@gmail.com>
2024-07-16 18:13:50 -0400

    puerto frond configurado

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 // Configura CORS para permitir solicitudes desde el frontend
 app.use(cors({
-  origin: 'http://localhost:3000', // Cambia esto a la URL de tu frontend en producción
+  origin: 'http://localhost:8080', // Cambia esto a la URL de tu frontend en producción
   credentials: true
 }));
 
